0516f1ae422added83b1a25b5e9d2549792b6d6a
onebarloop <onebarloop@posteo.de>
2026-09-05 13:49:05 +0200

    bugfix ts in drizzle

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,7 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from './schema';
+import { relations } from './schema';
 
 export const db = drizzle({
   connection: process.env.DATABASE_URL!,
-  schema: schema,
-  relations: schema.relations,
+  relations,
 });
