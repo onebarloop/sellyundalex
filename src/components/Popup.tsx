aa9ccf32196ca1a16f81f4900b3a9499c0a41496
onebarloop <onebarloop@posteo.de>
2026-09-25 20:26:58 +0200

    add blur effect transition in popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { motion, AnimatePresence } from 'motion/react';
 
 type Props = {
   trigger: ReactNode;
@@ -11,14 +12,27 @@ export default function Popup({ trigger, children, show, onClick }: Props) {
   return (
     <>
       {trigger}
-      {show && (
-        <div
-          className="w-dvw h-dvh top-0 left-0 bg-black/20 backdrop-blur-sm flex items-center justify-center fixed z-50"
-          onClick={onClick}
-        >
-          {children}
-        </div>
-      )}
+      <AnimatePresence>
+        {show && (
+          <div
+            className="w-dvw h-dvh top-0 left-0 flex items-center justify-center fixed z-50 "
+            onClick={onClick}
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="realtive z-10"
+            >
+              {children}
+            </motion.div>
+            <motion.div
+              className="bg-black/20 backdrop-blur-sm absolute inset-0 z-0"
+              exit={{ opacity: 0 }}
+            />
+          </div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
